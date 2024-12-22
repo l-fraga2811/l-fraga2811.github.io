810b2c8d8cf48d68b23b50ef51ace67d21263350
l-fraga2811 <lucasbelter2805@gmail.com>
2024-12-21 23:35:05 -0300

    portfolio commit, need to add projects

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -18,7 +17,6 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
         <Experience />
         <Footer />
       </div>
